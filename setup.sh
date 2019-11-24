2bdb97cc84f42ffbf4fc74844f18be82a0843ed2
freakie <freakie2007@gmail.com>
2019-11-24 17:48:51 +0800

    Add python 3.x install script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+yum install centos-release-scl
+yum install rh-python36
+scl enable rh-python36 bash
+
+echo "install prerequisite module"
+pip install --upgrade pip
+pip install --user --requirement requirements.txt
 echo "install prerequisite module"
 pip install --user --requirement requirements.txt
