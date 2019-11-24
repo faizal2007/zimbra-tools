433289ae006151d45698717e65babeb4466963e3
Faizal Sadri <freakie2007@gmail.com>
2019-11-25 00:23:13 +0800

    Update setup-centos7.sh
    remove python link to python 3.7 need to access direct access /usr/local/bin because yum require python 2.7

--- setup-centos7.sh
+++ setup-centos7.sh
@@ -3,7 +3,7 @@
 REQUIREMENT="$(pwd)/requirements.txt"
 
 echo "Installing Python 3.7"
-yum install gcc openssl-devel bzip2-devel libffi-devel wget
+yum install gcc openssl-devel bzip2-devel libffi-devel wget 
 cd /usr/src
 wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
 tar xzf Python-3.7.4.tgz
@@ -12,8 +12,8 @@ cd Python-3.7.4
 make install
 rm -rvf /usr/src/Python-3.7.4.tgz
 
-ln -sf /usr/local/bin/python3.7 /usr/bin/python
-export PATH=$PATH:/$USER/.local/bin:/usr/local/bin
+#ln -sf /usr/local/bin/python3.7 /usr/bin/python
+export PATH="$HOME/.local/bin:/usr/local/bin:$PATH"
 
 echo "Install python pip"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
